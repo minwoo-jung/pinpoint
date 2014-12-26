071a29897d4aee0a9b602ae1412211095ffdabcb
Woonduk Kang <wd.kang@navercorp.com>
2014-12-26 18:13:12 +0900

    modified @author koo.taejin
    Former-commit-id: 88e3e1c7bf628dbc6f5bf333bb2686ef586bcb80

--- naver-plugin/src/test/java/com/navercorp/pinpoint/profiler/modifier/arcus/FrontCacheGetFutureModifierIntegrationTest.java
+++ naver-plugin/src/test/java/com/navercorp/pinpoint/profiler/modifier/arcus/FrontCacheGetFutureModifierIntegrationTest.java
@@ -9,6 +9,7 @@ import net.spy.memcached.ConnectionFactoryBuilder;
 import org.junit.Before;
 import org.junit.Ignore;
 import org.junit.Test;
+import org.mockito.MockitoAnnotations;
 
 import java.util.List;
 
@@ -22,11 +23,11 @@ public class FrontCacheGetFutureModifierIntegrationTest extends BasePinpointTest
 
     @Before
     public void setUp() throws Exception {
-//        MockitoAnnotations.initMocks(this);
+        MockitoAnnotations.initMocks(this);
     }
 
     @Test
-    @Ignore
+//    @Ignore
     // FIXME 테스트 깨짐
     public void frontCacheShouldBeTraced() throws Exception {
         // given: front-cache-enabled ArcusClient
