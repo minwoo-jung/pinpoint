c4e35a81689396eae9ec76f243854f7353b2d358
Minwoo Jung <minwoo.jung@navercorp.com>
2015-01-06 16:12:26 +0900

    [#95] Add test code
    Former-commit-id: 027067066f188a3647106b5ecc768b31c511032b

--- testweb/src/main/java/com/navercorp/pinpoint/testweb/controller/HttpClient3Controller.java
+++ testweb/src/main/java/com/navercorp/pinpoint/testweb/controller/HttpClient3Controller.java
@@ -12,7 +12,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.navercorp.pinpoint.testweb.connector.apachehttp3.ApacheHttpClient3;
-import com.navercorp.pinpoint.testweb.util.Description;
 
 @Controller
 public class HttpClient3Controller {
@@ -28,7 +27,6 @@ public class HttpClient3Controller {
         return "OK";
     }
     
-    @Description("에러시 cookie덤프")
     @RequestMapping(value = "/httpclient3/cookie")
     @ResponseBody
     public String cookie(@RequestHeader(value = "Cookie", required = false) String cookie, HttpServletRequest request) {
@@ -40,19 +38,6 @@ public class HttpClient3Controller {
         return "OK";
     }
     
-    @Description("에러시 cookie덤프")
-    @RequestMapping(value = "/httpclient3/exception")
-    @ResponseBody
-    public String exception(@RequestHeader(value = "Cookie", required = false) String cookie, HttpServletRequest request) {
-        logger.info("Cookie:{}", cookie);
-
-        ApacheHttpClient3 client = new ApacheHttpClient3();
-        client.executeWithCookie("http://localhost:8090/asdf.pinpoint", new HashMap<String, Object>(), null);
-
-        return "OK";
-    }
-    
-    @Description("에러시 cookie덤프")
     @RequestMapping(value = "/httpclient3/post")
     @ResponseBody
     public String post(@RequestHeader(value = "Cookie", required = false) String cookie, HttpServletRequest request) {
@@ -62,14 +47,9 @@ public class HttpClient3Controller {
         client.executePost();
         
         return "OK";
-//        ApacheHttpClient4 client = new ApacheHttpClient4(new HttpConnectorOptions());
-//        client.execute("http://localhost:" + request.getLocalPort() + "/combination.pinpoint", new HashMap<String, Object>(), cookie);
-//
-//        return "OK";
     }
     
     
-    @Description("에러시 cookie덤프")
     @RequestMapping(value = "/httpclient3/remoteCall")
     @ResponseBody
     public String remoteCall(@RequestHeader(value = "Cookie", required = false) String cookie, HttpServletRequest request) {
