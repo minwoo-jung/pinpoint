cb555da17d127dac6c3ed59c50f6ee1302bd1c26
Minwoo Jung <minwoo.jung@navercorp.com>
2015-01-09 18:53:17 +0900

    [#6] Support to link monitor Site for multiple company/IDC location.
    Former-commit-id: a2fafe44b5d434d8df8bc1af654b8e034a835714

--- naver-web/src/main/java/com/navercorp/pinpoint/web/applicationmap/link/NaverMatcherGroup.java
+++ naver-web/src/main/java/com/navercorp/pinpoint/web/applicationmap/link/NaverMatcherGroup.java
@@ -1,14 +1,50 @@
 package com.navercorp.pinpoint.web.applicationmap.link;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+
 /**
  * @author minwoo.jung <minwoo.jung@navercorp.com>
- *
  */
 public class NaverMatcherGroup extends MatcherGroup {
     
-    public NaverMatcherGroup() {
-        addServerMatcher(new NSightMatcher());
-        addServerMatcher(new JapanNSightMatcher());
+    static final String MATCHER_KEY_PREFIX = "site.matcher.key";
+    static final String MATCHER_URL_PREFIX = "site.matcher.url";
+    static final String LINK_NAME ="Nsight";
+    
+    public NaverMatcherGroup(final Map<String, String> matcherProps) {
+        if (matcherProps != null) {
+            Map<String, String> keyMap = new HashMap<String, String>();
+            Map<String, String> urlMap = new HashMap<String, String>();
+            
+            for (Entry<String, String> matcherProp : matcherProps.entrySet()) {
+                if (matcherProp.getKey().startsWith(MATCHER_KEY_PREFIX)) {
+                    keyMap.put(matcherProp.getKey(), matcherProp.getValue());
+                } else if (matcherProp.getKey().startsWith(MATCHER_URL_PREFIX)) {
+                    urlMap.put(matcherProp.getKey(), matcherProp.getValue());
+                }
+            }
+            
+            int comNameStartIndex = MATCHER_KEY_PREFIX.length() + 1;
+            
+            for (Entry<String, String> keyInfo : keyMap.entrySet()) {
+                String comName = keyInfo.getKey().substring(comNameStartIndex, keyInfo.getKey().length());
+                String matcherUrlName = MATCHER_URL_PREFIX + "." + comName;
+                
+                if (comName.isEmpty() || urlMap.get(matcherUrlName) == null) {
+                    continue;
+                }
+       
+                String[] keys = keyInfo.getValue().replaceAll(" ", "").split(",");
+                
+                for (String key : keys) {
+                    addServerMatcher(new PostfixServerMatcher(key, urlMap.get(matcherUrlName), LINK_NAME));
+                }
+                
+            }
+        }
+
         setDefaultMatcher(new DefaultNSightMatcher());
     }
 
