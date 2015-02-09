c49ea8e0acb07fe2456a49fa4be4fcfcae6022fa
Minwoo Jung <minwoo.jung@navercorp.com>
2015-02-09 14:39:25 +0900

    [#143] Add log4j, logback profiler example code
    Former-commit-id: 03beae47f16f40d640ce4a686a7fed429567cd65

--- testweb/src/main/java/com/navercorp/pinpoint/testweb/controller/LoggingController.java
+++ testweb/src/main/java/com/navercorp/pinpoint/testweb/controller/LoggingController.java
@@ -5,6 +5,7 @@ import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
+import java.util.Date;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -18,6 +19,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.method.HandlerMethod;
 import org.springframework.web.servlet.mvc.method.RequestMappingInfo;
 import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
@@ -38,80 +40,32 @@ public class LoggingController {
 
 
     @RequestMapping(value = "/logging", method = RequestMethod.GET)
+    @ResponseBody
     public String logging(Model model) {
-        model.addAttribute("mapping", getMappingInfo());
-        return "docs";
+        mdcClear();
+        writeLog("logging.pinpoint api is called");
+        return "OK";
     }
     
     @RequestMapping(value = "/loggingWithMDC", method = RequestMethod.GET)
+    @ResponseBody
     public String logging2(Model model) {
+        mdcClear();
         settingMDCValue();
-        model.addAttribute("mapping", getMappingInfo());
-        return "docs";
+        writeLog("loggingWithMDC.pinpoint api is called");
+        return "OK";
     }
-
+    
+    private void mdcClear() {
+        MDC.clear();
+    }
 
     private void settingMDCValue() {
-        MDC.put("ProjectName", "pinpoint-testweb");
+        MDC.put("ProjectName", "pinpoint-testweb" + new Date());
     }
 
 
-    public Map<String, List<ControllerMappingInfo>> getMappingInfo() {
-        Map<String, List<ControllerMappingInfo>> info = new TreeMap<String, List<ControllerMappingInfo>>();
-
-        Map<RequestMappingInfo, HandlerMethod> handlerMethods = handlerMapping.getHandlerMethods();
-        for (Map.Entry<RequestMappingInfo, HandlerMethod> requestMappingInfoHandlerMethodEntry : handlerMethods.entrySet()) {
-            RequestMappingInfo requestMappingInfoKey = requestMappingInfoHandlerMethodEntry.getKey();
-            HandlerMethod handlerMethod = requestMappingInfoHandlerMethodEntry.getValue();
-            Method method = handlerMethod.getMethod();
-            Class<?> declaringClass = method.getDeclaringClass();
-
-            List<ControllerMappingInfo> controllerMappingInfoList = info.get(declaringClass.getSimpleName());
-            if (controllerMappingInfoList == null) {
-                controllerMappingInfoList = new ArrayList<ControllerMappingInfo>();
-                info.put(declaringClass.getSimpleName(), controllerMappingInfoList);
-            }
-
-            List<ControllerMappingInfo> requestInfo = createRequestMappingInfo(requestMappingInfoKey, handlerMethod);
-            controllerMappingInfoList.addAll(requestInfo);
-        }
-        sort(info);
-        logger.info(" {}", info);
-        return info;
-    }
-
-    private void sort(Map<String, List<ControllerMappingInfo>> info) {
-        for (List<ControllerMappingInfo> controllerMappingInfos : info.values()) {
-            Collections.sort(controllerMappingInfos, new Comparator<ControllerMappingInfo>() {
-                @Override
-                public int compare(ControllerMappingInfo o1, ControllerMappingInfo o2) {
-                    return o1.getUrl().compareTo(o2.getUrl());
-                }
-            });
-        }
-    }
-
-    private List<ControllerMappingInfo> createRequestMappingInfo(RequestMappingInfo requestMappingInfo, HandlerMethod handlerMethod) {
-        List<ControllerMappingInfo> requestInfo = new ArrayList<ControllerMappingInfo>();
-
-        Set<String> patterns = requestMappingInfo.getPatternsCondition().getPatterns();
-        for (String pattern : patterns) {
-            Description description = getDescription(handlerMethod);
-            ControllerMappingInfo info = new ControllerMappingInfo(pattern, (description == null) ? "" : description.value());
-            requestInfo.add(info);
-        }
-
-        return requestInfo;
-    }
-
-    private Description getDescription(HandlerMethod handlerMethod) {
-        Annotation[] annotations = handlerMethod.getMethod().getAnnotations();
-        for (Annotation annotation : annotations) {
-            if (annotation instanceof com.navercorp.pinpoint.testweb.util.Description) {
-                return (Description) annotation;
-            }
-        }
-
-        return null;
+    public void writeLog(String message) {
+        logger.info(message);
     }
 }
--- testweb/src/main/java/com/navercorp/pinpoint/testweb/log/appender/NeloThrfitAppender.java
+++
@@ -1,5 +0,0 @@
-package com.navercorp.pinpoint.testweb.log.appender;
-
-public class NeloThrfitAppender {
-
-}
