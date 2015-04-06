47e1a8b585dec19ce05c8050272258ea7450448d
Jongho Moon <jongho.moon@navercorp.com>
2015-04-06 14:26:51 +0900

    Renamed @Scope to @Group
    Former-commit-id: f9a34cf7e98c85032bd6479cfdfdd466351239c8

--- plugins/arcus/src/main/java/com/navercorp/pinpoint/plugin/arcus/interceptor/FutureGetInterceptor.java
+++ plugins/arcus/src/main/java/com/navercorp/pinpoint/plugin/arcus/interceptor/FutureGetInterceptor.java
@@ -15,7 +15,7 @@ import com.navercorp.pinpoint.bootstrap.logging.PLogger;
 import com.navercorp.pinpoint.bootstrap.logging.PLoggerFactory;
 import com.navercorp.pinpoint.bootstrap.plugin.Cached;
 import com.navercorp.pinpoint.bootstrap.plugin.Name;
-import com.navercorp.pinpoint.bootstrap.plugin.Scope;
+import com.navercorp.pinpoint.bootstrap.plugin.Group;
 import com.navercorp.pinpoint.bootstrap.plugin.TargetMethod;
 import com.navercorp.pinpoint.common.ServiceType;
 import com.navercorp.pinpoint.plugin.arcus.ArcusConstants;
@@ -24,7 +24,7 @@ import com.navercorp.pinpoint.plugin.arcus.ArcusConstants;
  * @author emeroad
  */
 @TargetMethod(name="get", paramTypes={"long", "java.util.concurrent.TimeUnit"})
-@Scope(ArcusConstants.ARCUS_SCOPE)
+@Group(ArcusConstants.ARCUS_SCOPE)
 public class FutureGetInterceptor implements SimpleAroundInterceptor, ArcusConstants {
 
     private final PLogger logger = PLoggerFactory.getLogger(this.getClass());
