64f7d5256edc51d99c09bb1a67c58663152c93b8
Jongho Moon <jongho.moon@navercorp.com>
2015-09-03 16:48:15 +0900

    BLOC plugin now uses redesigned plugin api
    Former-commit-id: 72d1b0887d04263501b587b1d341a89a23a5460c

---
+++ naver-plugins/bloc/src/main/java/com/navercorp/pinpoint/plugin/bloc/v3/Bloc3Plugin.java
@@ -0,0 +1,34 @@
+package com.navercorp.pinpoint.plugin.bloc.v3;
+
+import java.security.ProtectionDomain;
+
+import com.navercorp.pinpoint.bootstrap.instrument.InstrumentClass;
+import com.navercorp.pinpoint.bootstrap.instrument.InstrumentException;
+import com.navercorp.pinpoint.bootstrap.plugin.ProfilerPlugin;
+import com.navercorp.pinpoint.bootstrap.plugin.ProfilerPluginInstrumentContext;
+import com.navercorp.pinpoint.bootstrap.plugin.ProfilerPluginSetupContext;
+import com.navercorp.pinpoint.bootstrap.plugin.transformer.PinpointClassFileTransformer;
+import com.navercorp.pinpoint.plugin.bloc.BlocConstants;
+
+public class Bloc3Plugin implements ProfilerPlugin, BlocConstants {
+    
+    @Override
+    public void setup(ProfilerPluginSetupContext context) {
+        context.addApplicationTypeDetector(new Bloc3Detector());
+        
+        addBlocAdapterEditor(context);
+    }
+
+    private void addBlocAdapterEditor(ProfilerPluginSetupContext context) {
+        context.addClassFileTransformer("com.nhncorp.lucy.bloc.handler.HTTPHandler$BlocAdapter", new PinpointClassFileTransformer() {
+            
+            @Override
+            public byte[] transform(ProfilerPluginInstrumentContext instrumentContext, ClassLoader loader, String className, Class<?> classBeingRedefined, ProtectionDomain protectionDomain, byte[] classfileBuffer) throws InstrumentException {
+                InstrumentClass target = instrumentContext.getInstrumentClass(loader, className, classfileBuffer);
+                target.addInterceptor("com.navercorp.pinpoint.plugin.bloc.v3.interceptor.ExecuteMethodInterceptor");
+                return target.toBytecode();
+            }
+        });
+    }
+    
+}
--- naver-plugins/bloc/src/main/java/com/navercorp/pinpoint/plugin/bloc/v4/Bloc4Plugin.java
+++ naver-plugins/bloc/src/main/java/com/navercorp/pinpoint/plugin/bloc/v4/Bloc4Plugin.java
@@ -1,52 +1,74 @@
-package com.navercorp.pinpoint.plugin.bloc;
+package com.navercorp.pinpoint.plugin.bloc.v4;
 
+import java.security.ProtectionDomain;
+
+import com.navercorp.pinpoint.bootstrap.instrument.InstrumentClass;
+import com.navercorp.pinpoint.bootstrap.instrument.InstrumentException;
 import com.navercorp.pinpoint.bootstrap.plugin.ProfilerPlugin;
+import com.navercorp.pinpoint.bootstrap.plugin.ProfilerPluginInstrumentContext;
 import com.navercorp.pinpoint.bootstrap.plugin.ProfilerPluginSetupContext;
-import com.navercorp.pinpoint.bootstrap.plugin.transformer.ClassFileTransformerBuilder;
-import com.navercorp.pinpoint.plugin.bloc.v3.Bloc3Detector;
-import com.navercorp.pinpoint.plugin.bloc.v4.Bloc4Detector;
+import com.navercorp.pinpoint.bootstrap.plugin.transformer.PinpointClassFileTransformer;
+import com.navercorp.pinpoint.plugin.bloc.BlocConstants;
 
-public class BlocPlugin implements ProfilerPlugin, BlocConstants {
+public class Bloc4Plugin implements ProfilerPlugin, BlocConstants {
     
     @Override
     public void setup(ProfilerPluginSetupContext context) {
-        context.addApplicationTypeDetector(new Bloc3Detector(), new Bloc4Detector());
+        context.addApplicationTypeDetector(new Bloc4Detector());
         
-        addBlocAdapterEditor(context);
         addNettyInboundHandlerModifier(context);
         addNpcHandlerModifier(context);
         addRequestProcessorModifier(context);
         addModuleClassLoaderFactoryInterceptor(context);
     }
 
-    private void addBlocAdapterEditor(ProfilerPluginSetupContext context) {
-        ClassFileTransformerBuilder builder = context.getClassFileTransformerBuilder("com.nhncorp.lucy.bloc.handler.HTTPHandler$BlocAdapter");
-        builder.injectInterceptor("com.navercorp.pinpoint.plugin.bloc.v3.interceptor.ExecuteMethodInterceptor");
-        context.addClassFileTransformer(builder.build());
-    }
-    
     private void addNettyInboundHandlerModifier(ProfilerPluginSetupContext context) {
-        ClassFileTransformerBuilder builder = context.getClassFileTransformerBuilder("com.nhncorp.lucy.bloc.http.NettyInboundHandler");
-        builder.injectFieldAccessor(FIELD_URI_ENCODING);
-        builder.injectInterceptor("com.navercorp.pinpoint.plugin.bloc.v4.interceptor.ChannelRead0Interceptor");
-        context.addClassFileTransformer(builder.build());
+        context.addClassFileTransformer("com.nhncorp.lucy.bloc.http.NettyInboundHandler", new PinpointClassFileTransformer() {
+            
+            @Override
+            public byte[] transform(ProfilerPluginInstrumentContext instrumentContext, ClassLoader loader, String className, Class<?> classBeingRedefined, ProtectionDomain protectionDomain, byte[] classfileBuffer) throws InstrumentException {
+                InstrumentClass target = instrumentContext.getInstrumentClass(loader, className, classfileBuffer);
+                
+                target.addGetter("com.navercorp.pinpoint.plugin.bloc.v4.UriEncodingGetter", "uriEncoding");
+                target.addInterceptor("com.navercorp.pinpoint.plugin.bloc.v4.interceptor.ChannelRead0Interceptor");
+                return target.toBytecode();
+            }
+        });
     }    
     
     private void addNpcHandlerModifier(ProfilerPluginSetupContext context) {
-        ClassFileTransformerBuilder builder = context.getClassFileTransformerBuilder("com.nhncorp.lucy.bloc.npc.handler.NpcHandler");
-        builder.injectInterceptor("com.navercorp.pinpoint.plugin.bloc.v4.interceptor.MessageReceivedInterceptor");
-        context.addClassFileTransformer(builder.build());
+        context.addClassFileTransformer("com.nhncorp.lucy.bloc.npc.handler.NpcHandler", new PinpointClassFileTransformer() {
+            
+            @Override
+            public byte[] transform(ProfilerPluginInstrumentContext instrumentContext, ClassLoader loader, String className, Class<?> classBeingRedefined, ProtectionDomain protectionDomain, byte[] classfileBuffer) throws InstrumentException {
+                InstrumentClass target = instrumentContext.getInstrumentClass(loader, className, classfileBuffer);
+                target.addInterceptor("com.navercorp.pinpoint.plugin.bloc.v4.interceptor.MessageReceivedInterceptor");
+                return target.toBytecode();
+            }
+        });
     }
 
     private void addRequestProcessorModifier(ProfilerPluginSetupContext context) {
-        ClassFileTransformerBuilder builder = context.getClassFileTransformerBuilder("com.nhncorp.lucy.bloc.core.processor.RequestProcessor");
-        builder.injectInterceptor("com.navercorp.pinpoint.plugin.bloc.v4.interceptor.ProcessInterceptor");
-        context.addClassFileTransformer(builder.build());
+        context.addClassFileTransformer("com.nhncorp.lucy.bloc.core.processor.RequestProcessor", new PinpointClassFileTransformer() {
+            
+            @Override
+            public byte[] transform(ProfilerPluginInstrumentContext instrumentContext, ClassLoader loader, String className, Class<?> classBeingRedefined, ProtectionDomain protectionDomain, byte[] classfileBuffer) throws InstrumentException {
+                InstrumentClass target = instrumentContext.getInstrumentClass(loader, className, classfileBuffer);
+                target.addInterceptor("com.navercorp.pinpoint.plugin.bloc.v4.interceptor.ProcessInterceptor");
+                return target.toBytecode();
+            }
+        });
     }
     
     private void addModuleClassLoaderFactoryInterceptor(ProfilerPluginSetupContext context) {
-        ClassFileTransformerBuilder builder = context.getClassFileTransformerBuilder("com.nhncorp.lucy.bloc.core.clazz.ModuleClassLoaderFactory");
-        builder.injectInterceptor("com.navercorp.pinpoint.plugin.bloc.v4.interceptor.ModuleClassLoaderFactoryInterceptor");
-        context.addClassFileTransformer(builder.build());
+        context.addClassFileTransformer("com.nhncorp.lucy.bloc.core.clazz.ModuleClassLoaderFactory", new PinpointClassFileTransformer() {
+            
+            @Override
+            public byte[] transform(ProfilerPluginInstrumentContext instrumentContext, ClassLoader loader, String className, Class<?> classBeingRedefined, ProtectionDomain protectionDomain, byte[] classfileBuffer) throws InstrumentException {
+                InstrumentClass target = instrumentContext.getInstrumentClass(loader, className, classfileBuffer);
+                target.addInterceptor("com.navercorp.pinpoint.plugin.bloc.v4.interceptor.ModuleClassLoaderFactoryInterceptor");
+                return target.toBytecode();
+            }
+        });
     }
 }
