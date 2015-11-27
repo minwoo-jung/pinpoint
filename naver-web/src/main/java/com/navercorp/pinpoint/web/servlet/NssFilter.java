63e373849d45e554b19441b1b8249978a724d4b7
hyungil.jeong <hyungil.jeong@navercorp.com>
2015-11-27 16:28:50 +0900

    #394 Add filter to check for nss user authorization
    Former-commit-id: 164dd5f8e08b1619f3de63ac4b472c617a3e7c4e

---
+++ naver-web/src/main/java/com/navercorp/pinpoint/web/servlet/NssFilter.java
@@ -0,0 +1,127 @@
+/*
+ * Copyright 2015 NAVER Corp.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.navercorp.pinpoint.web.servlet;
+
+import org.apache.commons.collections.CollectionUtils;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.util.StringUtils;
+
+import javax.annotation.PostConstruct;
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+import java.util.*;
+
+/**
+ * @author HyunGil Jeong
+ */
+public class NssFilter implements Filter {
+
+    private FilterConfig filterConfig;
+
+    @Value("#{pinpointWebProps['nss.user.header.key'] ?: 'SSO_USER'}")
+    private String userHeaderKey;
+
+    @Value("#{pinpointWebProps['nss.override.id'] ?: ''}")
+    private String overrideIdVal;
+
+    @Value("#{pinpointWebProps['nss.corportaion.prefix'] ?: ''}")
+    private String acceptedCorporationPrefixes;
+
+    @Value("#{pinpointWebProps['nss.user.type'] ?: ''}")
+    private String acceptedUserTypes;
+
+    private Set<String> overrideIds;
+    private List<String> acceptedPrefixes;
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        this.filterConfig = filterConfig;
+    }
+
+    @PostConstruct
+    public void init() {
+        if (StringUtils.isEmpty(this.overrideIdVal)) {
+            this.overrideIds = Collections.emptySet();
+        } else {
+            String[] overrideIds = this.overrideIdVal.split(",");
+            if (overrideIds.length == 0) {
+                this.overrideIds = Collections.emptySet();
+            } else {
+                this.overrideIds = new HashSet<>(overrideIds.length);
+                for (String overrideId : overrideIds) {
+                    this.overrideIds.add(overrideId.toUpperCase());
+                }
+            }
+        }
+        if (StringUtils.isEmpty(this.acceptedCorporationPrefixes)) {
+            this.acceptedPrefixes = Collections.emptyList();
+        } else {
+            this.acceptedPrefixes = new ArrayList<>();
+            final String[] prefixes = this.acceptedCorporationPrefixes.split(",");
+            final String[] types = this.acceptedUserTypes.split(",");
+            if (types.length == 0) {
+                for (String prefix : prefixes) {
+                    this.acceptedPrefixes.add(prefix);
+                }
+            } else {
+                for (String prefix : prefixes) {
+                    for (String type : types) {
+                        this.acceptedPrefixes.add(prefix + type);
+                    }
+                }
+            }
+        }
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest httpServletRequest = (HttpServletRequest) request;
+        String userId = httpServletRequest.getHeader(userHeaderKey);
+        if (userId == null) {
+            chain.doFilter(request, response);
+        } else {
+            if (isAllowed(userId.toUpperCase())) {
+                chain.doFilter(request, response);
+            } else {
+                RequestDispatcher requestDispatcher = httpServletRequest.getRequestDispatcher("/not_authorized.html");
+                requestDispatcher.forward(request, response);
+            }
+        }
+    }
+
+    @Override
+    public void destroy() {
+        this.filterConfig = null;
+    }
+
+    private boolean isAllowed(String userId) {
+        if (this.overrideIds.contains(userId)) {
+            return true;
+        }
+        if (CollectionUtils.isEmpty(this.acceptedPrefixes)) {
+            return true;
+        } else {
+            for (String acceptedPrefix : this.acceptedPrefixes) {
+                if (userId.startsWith(acceptedPrefix)) {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
---
+++ naver-web/src/test/java/com/navercorp/pinpoint/web/servlet/NssFilterTest.java
@@ -0,0 +1,166 @@
+/*
+ * Copyright 2015 NAVER Corp.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.navercorp.pinpoint.web.servlet;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.mock.web.MockRequestDispatcher;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.RequestDispatcher;
+
+/**
+ * @author HyunGil Jeong
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("classpath:applicationContext-test.xml")
+public class NssFilterTest {
+    /*
+    # NSS Configuration
+    nss.user.header.key=SSO_USER
+    nss.override.id=kr99999
+    # Allowed prefixes
+    nss.corportaion.prefix=KR,NB,NT,IT,CA,WM,LP,JP,LZ
+    nss.user.type=1,7
+     */
+    private static final String AUTHORIZED_USER_ID_UPPER = "KR10000";
+    private static final String AUTHORIZED_USER_ID_LOWER = "kr10000";
+
+    private static final String UNAUTHORIZED_USER_ID_CORP = "AA00000";
+    private static final String UNAUTHORIZED_USER_ID_TYPE = "kr50000";
+
+    private static final String EMPTY_USER_ID = "";
+    private static final String OVERRIDDEN_USER_ID = "kr99999";
+
+    @Value("#{pinpointWebProps['nss.user.header.key'] ?: 'SSO_USER'}")
+    private String userHeaderKey;
+
+    private MockHttpServletRequest request;
+    private MockHttpServletResponse response;
+
+    @Mock
+    private FilterChain filterChain;
+
+    @Autowired
+    private Filter nssFilter;
+
+    @Before
+    public void setUp() throws Exception {
+        MockitoAnnotations.initMocks(this);
+        this.request = new MockHttpServletRequest() {
+            @Override
+            public RequestDispatcher getRequestDispatcher(String path) {
+                return new MockRequestDispatcher("/not_authorized.html");
+            }
+        };
+        this.response = new MockHttpServletResponse();
+        doNothing().when(this.filterChain).doFilter(this.request, this.response);
+    }
+
+    @Test
+    public void authorized_user_should_pass_upper() throws Exception {
+        // Given
+        this.request.addHeader(this.userHeaderKey, AUTHORIZED_USER_ID_UPPER);
+        // When
+        this.nssFilter.doFilter(this.request, this.response, this.filterChain);
+        // Then
+        assertAuthorized();
+    }
+
+    @Test
+    public void authorized_user_should_pass_lower() throws Exception {
+        // Given
+        this.request.addHeader(this.userHeaderKey, AUTHORIZED_USER_ID_LOWER);
+        // When
+        this.nssFilter.doFilter(this.request, this.response, this.filterChain);
+        // Then
+        assertAuthorized();
+    }
+
+    @Test
+    public void unauthorized_corporate_user_should_be_redirected() throws Exception {
+        // Given
+        this.request.addHeader(this.userHeaderKey, UNAUTHORIZED_USER_ID_CORP);
+        // When
+        this.nssFilter.doFilter(this.request, this.response, this.filterChain);
+        // Then
+        assertUnauthorized();
+    }
+
+    @Test
+    public void unauthorized_user_type_should_be_redirected() throws Exception {
+        // Given
+        this.request.addHeader(this.userHeaderKey, UNAUTHORIZED_USER_ID_TYPE);
+        // When
+        this.nssFilter.doFilter(this.request, this.response, this.filterChain);
+        // Then
+        assertUnauthorized();
+    }
+
+    @Test
+    public void missing_header_should_pass() throws Exception {
+        // Given
+        // When
+        this.nssFilter.doFilter(this.request, this.response, this.filterChain);
+        // Then
+        assertAuthorized();
+    }
+
+    @Test
+    public void empty_header_value_should_be_redirected() throws Exception {
+        // Given
+        this.request.addHeader(this.userHeaderKey, EMPTY_USER_ID);
+        // When
+        this.nssFilter.doFilter(this.request, this.response, this.filterChain);
+        // Then
+        assertUnauthorized();
+    }
+
+    @Test
+    public void unauthorized_but_override_users_should_pass() throws Exception {
+        // Given
+        this.request.addHeader(this.userHeaderKey, OVERRIDDEN_USER_ID);
+        // When
+        this.nssFilter.doFilter(this.request, this.response, this.filterChain);
+        // Then
+        assertAuthorized();
+    }
+
+    private void assertAuthorized() throws Exception {
+        verify(this.filterChain, times(1)).doFilter(this.request, this.response);
+        assertNull(this.response.getForwardedUrl());
+    }
+
+    private void assertUnauthorized() throws Exception {
+        verifyZeroInteractions(this.filterChain);
+        assertEquals("/not_authorized.html", this.response.getForwardedUrl());
+    }
+
+}
