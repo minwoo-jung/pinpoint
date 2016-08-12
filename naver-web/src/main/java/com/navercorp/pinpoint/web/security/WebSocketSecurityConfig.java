ee1c14dd26ba36d826157bea56d11b30a40a328c
minwoo.jung <minwoo.jung@navercorp.com>
2016-08-12 11:02:58 +0900

    [#693] code clean up

--- naver-web/src/main/java/com/navercorp/pinpoint/web/security/WebSocketSecurityConfig.java
+++ naver-web/src/main/java/com/navercorp/pinpoint/web/security/WebSocketSecurityConfig.java
@@ -2,15 +2,10 @@ package com.navercorp.pinpoint.web.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.messaging.simp.config.ChannelRegistration;
-import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.security.config.annotation.web.messaging.MessageSecurityMetadataSourceRegistry;
 import org.springframework.security.config.annotation.web.socket.AbstractSecurityWebSocketMessageBrokerConfigurer;
-import org.springframework.security.messaging.access.intercept.ChannelSecurityInterceptor;
-import org.springframework.security.messaging.context.SecurityContextChannelInterceptor;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
-import org.springframework.web.socket.config.annotation.WebSocketTransportRegistration;
 import org.springframework.web.socket.server.HandshakeInterceptor;
 
 
