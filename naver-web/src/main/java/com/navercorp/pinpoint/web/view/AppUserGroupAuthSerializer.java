6c9bd8ab2144ca2be80dd381267d93088522370d
minwoo.jung <minwoo.jung@navercorp.com>
2016-11-22 16:06:21 +0900

    [#NO-ISSUE]  klockwork

--- naver-web/src/main/java/com/navercorp/pinpoint/web/view/AppUserGroupAuthSerializer.java
+++ naver-web/src/main/java/com/navercorp/pinpoint/web/view/AppUserGroupAuthSerializer.java
@@ -7,25 +7,27 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonSerializer;
 import com.fasterxml.jackson.databind.SerializerProvider;
 import com.navercorp.pinpoint.web.vo.AppUserGroupAuth;
+import com.navercorp.pinpoint.web.vo.AppUserGroupAuth.Role;
 
 public class AppUserGroupAuthSerializer extends JsonSerializer<AppUserGroupAuth> {
 
     @Override
     public void serialize(AppUserGroupAuth appAuth, JsonGenerator jgen, SerializerProvider serializers)
-            throws IOException, JsonProcessingException {
+        throws IOException, JsonProcessingException {
         jgen.writeStartObject();
         jgen.writeStringField("applicationId", appAuth.getApplicationId());
         jgen.writeStringField("userGroupId", appAuth.getUserGroupId());
         jgen.writeObjectField("configuration", appAuth.getConfiguration());
 
-        String role = "";
-        if (appAuth.getRole() != null) {
-            role = appAuth.getRole().toString();
+        String roleName = "";
+        Role role = appAuth.getRole();
+        if (role != null) {
+            roleName = role.toString();
         }
-        jgen.writeStringField("role", role);
+        jgen.writeStringField("role", roleName);
 
         jgen.writeEndObject();
-        
+
     }
 
 }
