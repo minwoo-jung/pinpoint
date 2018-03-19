d9f83519472cddbddc9829b73100ee8e7fcd4e11
hyungil.jeong <hyungil.jeong@navercorp.com>
2018-03-19 13:48:45 +0900

    [#1768] Use PaaS hbase table name provider

---
+++ naver-web/src/main/java/com/navercorp/pinpoint/web/namespace/hbase/PaaSHbaseTableNameProvider.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright 2018 NAVER Corp.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.navercorp.pinpoint.web.namespace.hbase;
+
+import com.navercorp.pinpoint.common.hbase.TableNameProvider;
+import com.navercorp.pinpoint.common.hbase.util.HbaseTableNameCache;
+import com.navercorp.pinpoint.web.namespace.NameSpaceInfo;
+import com.navercorp.pinpoint.web.namespace.PaaSNameSpaceInfoFactory;
+import org.apache.hadoop.hbase.TableName;
+import org.springframework.util.StringUtils;
+
+import java.util.Objects;
+
+/**
+ * @author HyunGil Jeong
+ */
+public class PaaSHbaseTableNameProvider implements TableNameProvider {
+
+    private static final HbaseTableNameCache CACHE = new HbaseTableNameCache();
+
+    private final PaaSNameSpaceInfoFactory paaSNameSpaceInfoFactory;
+
+    public PaaSHbaseTableNameProvider(PaaSNameSpaceInfoFactory paaSNameSpaceInfoFactory) {
+        this.paaSNameSpaceInfoFactory = Objects.requireNonNull(paaSNameSpaceInfoFactory, "paaSNameSpaceInfoFactory must not be null");
+    }
+
+    @Override
+    public TableName getTableName(String tableName) {
+        NameSpaceInfo nameSpaceInfo = paaSNameSpaceInfoFactory.getNameSpaceInfo();
+        if (nameSpaceInfo == null) {
+            throw new IllegalStateException("NameSpaceInfo should not be null");
+        }
+        String namespace = nameSpaceInfo.getHbaseNamespace();
+        if (StringUtils.isEmpty(namespace)) {
+            throw new IllegalStateException("Hbase namespace should have been set");
+        }
+        return CACHE.get(namespace, tableName);
+    }
+}
