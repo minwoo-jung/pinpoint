0ccaf4a80417dcc7194fe0db6439b929af774a92
hyojong-song <hyojong.song2015@navercorp.com>
2018-05-15 15:00:12 +0900

    [#noissue] update yobi url.
    - https://yobi.navercorp.com/pinpoint/posts/263#1526361805472

--- naver-web/src/main/webapp/features/navbar/yobi.directive.js
+++ naver-web/src/main/webapp/features/navbar/yobi.directive.js
@@ -12,10 +12,10 @@
 			return {
 				template: [
 					'<div style="display:inline-block">',
-						'<a href="https://yobi.navercorp.com/Labs-public_pinpoint-issues/posts" target="_blank" style="color:#FFF;" ng-click="openNotice()">',
-							'<i class="xi-yobi" style="cursor:pointer;font-size:22px;"></i>',
-							'<span class="glyphicon glyphicon-exclamation-sign" style="font-size:14px;top:-12px;left:-10px;color:#71FF1F" ng-show="hasNotice"></span>',
-						'</a>',
+					'<a href="https://yobi.navercorp.com/Labs-public_pinpoint-issues/posts" target="_blank" style="color:#FFF;" ng-click="openNotice()">',
+					'<i class="xi-yobi" style="cursor:pointer;font-size:22px;"></i>',
+					'<span class="glyphicon glyphicon-exclamation-sign" style="font-size:14px;top:-12px;left:-10px;color:#71FF1F" ng-show="hasNotice"></span>',
+					'</a>',
 					'</div>'
 				].join(""),
 				scope: {},
@@ -23,22 +23,19 @@
 				link: function ( scope ) {
 					var aNoticeData = [];
 					scope.hasNotice = true;
-					$http.get( "http://yobiadmin.navercorp.com:8080/posts/pinpoint-issues?notice=true" ).success(function( aResult ) {
+					$http.get( "https://yobi.navercorp.com/api/projects/pinpoint/posts?boardId=0a70564d-5c5f-16e7-815c-5f06e84c006f" ).success(function( aResult ) {
 						var lastNoticeTime = webStorage.get("last-notice-time") || -1;
 
-						aNoticeData = aResult;
-						if ( aResult.length > 0 ) {
+						aNoticeData = aResult.elements;
+						if ( aNoticeData.length > 0 ) {
 							if ( lastNoticeTime === -1 ) {
 								scope.hasNotice = true;
 							} else {
-								var aOld = lastNoticeTime.split("-");
-								var aNew = aNoticeData[ aNoticeData.length - 1].createdDate.split("-");
-
-								for ( var i = 0 ; i < aOld.length ; i++ ) {
-									if  ( parseInt( aNew[i] ) > parseInt( aOld[i] ) ) {
-										scope.hasNotice = true;
-										break;
-									}
+								var newNoticeTime = aNoticeData[0].lastUpdatedAt;
+								if  ( newNoticeTime > lastNoticeTime ) {
+									scope.hasNotice = true;
+								} else {
+									scope.hasNotice = false;
 								}
 							}
 						}
@@ -46,9 +43,8 @@
 
 					});
 					scope.openNotice = function() {
-						console.log( "openNotice");
 						if ( aNoticeData.length > 0 ) {
-							webStorage.add("last-notice-time", aNoticeData[aNoticeData.length - 1].createdDate);
+							webStorage.add("last-notice-time", aNoticeData[0].lastUpdatedAt);
 						}
 						analyticsService.send( analyticsService.CONST.MAIN, "ClickOpenYobi" );
 						scope.hasNotice = false;
