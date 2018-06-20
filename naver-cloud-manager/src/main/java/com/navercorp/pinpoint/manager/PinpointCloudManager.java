cb24f6a63566091339155f878b0c888819cd14ee
minwoo.jung <minwoo.jung@navercorp.com>
2018-06-20 18:21:13 +0900

    [#1861] add create repository logic by liquibase

--- naver-cloud-manager/src/main/java/com/navercorp/pinpoint/manager/PinpointCloudManager.java
+++ naver-cloud-manager/src/main/java/com/navercorp/pinpoint/manager/PinpointCloudManager.java
@@ -16,9 +16,28 @@
 
 package com.navercorp.pinpoint.manager;
 
+import static io.undertow.predicate.Predicates.not;
+import static io.undertow.predicate.Predicates.path;
+
+import io.undertow.predicate.Predicate;
+import io.undertow.server.HandlerWrapper;
+import io.undertow.server.HttpHandler;
+import io.undertow.server.HttpServerExchange;
+import io.undertow.server.handlers.PredicateHandler;
+import io.undertow.server.handlers.resource.*;
+import io.undertow.servlet.api.DeploymentInfo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.embedded.undertow.UndertowBuilderCustomizer;
+import org.springframework.boot.context.embedded.undertow.UndertowDeploymentInfoCustomizer;
+import org.springframework.boot.context.embedded.undertow.UndertowEmbeddedServletContainerFactory;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ImportResource;
+import org.springframework.web.servlet.DispatcherServlet;
+
+import java.io.IOException;
+import java.nio.file.Paths;
 
 /**
  * @author HyunGil Jeong
@@ -30,4 +49,55 @@ public class PinpointCloudManager {
     public static void main(String[] args) {
         SpringApplication.run(PinpointCloudManager.class, args);
     }
+
+    @Bean
+    public ServletRegistrationBean dispatchServletRegistration(DispatcherServlet dispatcherServlet) {
+        ServletRegistrationBean registration = new ServletRegistrationBean(dispatcherServlet, "*.pinpoint");
+        return registration;
+    }
+
+
+    //static resource를 다룰 일이 생기면 사용하면 됨.
+    //static resource는 Defaultservlet 자체를 통하지 않고 handler 레벨에서 반환을 해주는게 가장 좋을듯함.
+    //그래야 인증 로직 자체를 거치지 않게 된다.
+    //web-inf와 meta-inf 경로를 다루는 로직은 아직 미완성이고, 현재 resources 폴더에 static 파일이 있는것도 개선이 필요함.
+//    @Bean
+//    public UndertowEmbeddedServletContainerFactory embeddedServletContainerFactory() {
+//        UndertowEmbeddedServletContainerFactory factory = new UndertowEmbeddedServletContainerFactory();
+//
+//        UndertowDeploymentInfoCustomizer customizer = new UndertowDeploymentInfoCustomizer() {
+//            @Override
+//            public void customize(DeploymentInfo deploymentInfo) {
+//                deploymentInfo.addInitialHandlerChainWrapper(new HandlerWrapper() {
+//                    @Override
+//                    public HttpHandler wrap(HttpHandler baseHandler) {
+//                        final ResourceManager resourceManager = new ClassPathResourceManager(this.getClass().getClassLoader(), "webapp/static/");
+//                        final ResourceHandler resourceHandler = new ResourceHandler(resourceManager);
+//                        resourceHandler.setDirectoryListingEnabled(false).setAllowed((not(path("META-INF"))));
+//                        Predicate predicate = new Predicate() {
+//                            @Override
+//                            public boolean resolve(HttpServerExchange value) {
+//                                try {
+//                                    Resource resource = resourceManager.getResource(value.getRelativePath());
+//                                    if (resource == null) {
+//                                        return false;
+//                                    }
+//                                    return true;
+//                                } catch (IOException ex) {
+//                                    return false;
+//                                }
+//                            }
+//                        };
+//
+//                        return new PredicateHandler(predicate, resourceHandler, baseHandler);
+//                    }
+//                });
+//            }
+//        };
+//
+//        factory.addDeploymentInfoCustomizers(customizer);
+//
+//        return factory;
+//    }
+
 }
