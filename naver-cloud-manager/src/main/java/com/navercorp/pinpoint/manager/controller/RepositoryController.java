c71966c84ddcbb4780dc450261a01d6fbc605ccd
emeroad <wd.kang@navercorp.com>
2018-10-17 18:42:46 +0900

    [#4751] Update library  - hbase client 1.2.6.1->1.2.7

--- naver-cloud-manager/src/main/java/com/navercorp/pinpoint/manager/controller/RepositoryController.java
+++ naver-cloud-manager/src/main/java/com/navercorp/pinpoint/manager/controller/RepositoryController.java
@@ -5,7 +5,7 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -17,13 +17,9 @@ package com.navercorp.pinpoint.manager.controller;
 
 import com.navercorp.pinpoint.manager.service.MetadataService;
 import com.navercorp.pinpoint.manager.service.RepositoryService;
-import org.apache.hadoop.hbase.shaded.org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
-import org.springframework.stereotype.Controller;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
