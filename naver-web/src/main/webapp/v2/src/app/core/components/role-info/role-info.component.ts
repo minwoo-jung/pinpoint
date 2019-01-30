261afec626b7e056694979a0c885daf628a1896e
hyojong-song <hyojong.song2015@navercorp.com>
2019-01-30 11:20:28 +0900

    [#1861] Change some radio element to checkbox in permmission page.

--- naver-web/src/main/webapp/v2/src/app/core/components/role-info/role-info.component.ts
+++ naver-web/src/main/webapp/v2/src/app/core/components/role-info/role-info.component.ts
@@ -1,28 +1,17 @@
-import { Component, OnInit, Input, OnChanges, SimpleChanges } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
     selector: 'pp-role-info',
     templateUrl: './role-info.component.html',
     styleUrls: ['./role-info.component.css']
 })
-export class RoleInfoComponent implements OnInit, OnChanges {
+export class RoleInfoComponent implements OnInit {
     @Input() roleInfo: IPermissions;
     @Input() i18nText: {[key: string]: string};
     disableFormControl = true;
-    permsGroupAppAuthorization = 'false';
-    permsGroupAlarm = 'false';
     permsGroupUserGroup = 'false';
 
     constructor() {}
     ngOnInit() {
-        this.setPermission();
-    }
-    ngOnChanges(changes: SimpleChanges) {
-        this.setPermission();
-    }
-    private setPermission() {
-        this.permsGroupAppAuthorization = this.roleInfo.permissionCollection.permsGroupAppAuthorization.editAuthorOnlyManager + '';
-        this.permsGroupAlarm = this.roleInfo.permissionCollection.permsGroupAlarm.editAuthorForEverything + '';
-        this.permsGroupUserGroup = this.roleInfo.permissionCollection.permsGroupUserGroup.editGroupOnlyGroupMember + '';
     }
 }
