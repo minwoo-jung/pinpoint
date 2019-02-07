afab719f14a46b1356dbfea64e0afbe248274be1
hyojong-song <hyojong.song2015@navercorp.com>
2019-02-07 15:40:54 +0900

    [#noissue] add config page route. - add route setting when empty sub-url.

--- naver-web/src/main/webapp/v2/src/app/routes/config-page/config-page.routing.ts
+++ naver-web/src/main/webapp/v2/src/app/routes/config-page/config-page.routing.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { ConfigPageComponent } from './config-page.component';
-import { UrlPathId } from 'app/shared/models';
+import { UrlPath, UrlPathId } from 'app/shared/models';
 import { UserConfigurationResolverService, ApplicationListResolverService, SystemConfigurationResolverService } from 'app/shared/services';
 import { ConfigurationGeneralContainerComponent } from 'app/core/components/configuration-general/configuration-general-container.component';
 import { ConfigurationUserGroupContainerComponent } from 'app/core/components/configuration-user-group/configuration-user-group-container.component';
@@ -49,6 +49,11 @@ const routes: Routes = [
             {
                 path: UrlPathId.HELP,
                 component: ConfigurationHelpContainerComponent
+            },
+            {
+                path: '',
+                redirectTo: '/' + UrlPath.CONFIG + '/' + UrlPathId.GENERAL,
+                pathMatch: 'full'
             }
         ]
     }
