503252c925ab27cc3aa0dc646a95965cf64670bf
minwoo.jung <minwoo.jung@navercorp.com>
2019-02-11 19:01:28 +0900

    [#1861] add table schema table role_definition, user_role

--- naver-web/src/main/resources/sql/Pinpoint-Schema.sql
+++ naver-web/src/main/resources/sql/Pinpoint-Schema.sql
@@ -30,4 +30,20 @@ CREATE TABLE `user_configuration` (
 
 ALTER TABLE user_configuration ADD UNIQUE KEY user_id_idx (user_id);
 
+CREATE TABLE `role_definition` (
+  `number` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `role_id` varchar(30) NOT NULL,
+  `permission_collection` text NOT NULL,
+  PRIMARY KEY (`number`)
+);
 
+ALTER TABLE role_definition ADD UNIQUE KEY role_id_idx (role_id);
+
+CREATE TABLE `user_role` (
+  `number` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `user_id` varchar(90) DEFAULT NULL,
+  `role_id` varchar(30) DEFAULT NULL,
+  PRIMARY KEY (`number`)
+);
+
+ALTER TABLE user_role ADD UNIQUE KEY user_id_role_id_idx (user_id, role_id);
