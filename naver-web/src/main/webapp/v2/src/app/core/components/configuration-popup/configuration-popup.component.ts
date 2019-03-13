b6e0927ef1a58eb0f1b9a2d9841772482d9be17c
QooQooDass <bindong.kim@navercorp.com>
2019-03-13 17:32:43 +0900

    [#2454] Enhance the config page UI

--- naver-web/src/main/webapp/v2/src/app/core/components/configuration-popup/configuration-popup.component.ts
+++ naver-web/src/main/webapp/v2/src/app/core/components/configuration-popup/configuration-popup.component.ts
@@ -1,16 +1,42 @@
 import { Component, OnInit, HostBinding, Output, EventEmitter, Input } from '@angular/core';
-import { BaseFloatingFilterChange } from 'ag-grid/dist/lib/filter/floatingFilter';
+import { trigger, state, style, animate, transition } from '@angular/animations';
 
 @Component({
     selector: 'pp-configuration-popup',
     templateUrl: './configuration-popup.component.html',
-    styleUrls: ['./configuration-popup.component.css']
+    styleUrls: ['./configuration-popup.component.css'],
+    animations: [
+        trigger('collapseSpread', [
+            state('collapsed', style({
+                maxHeight: 0,
+                overflow: 'hidden'
+            })),
+            state('spreaded', style({
+                maxHeight: '300px'
+            })),
+            transition('collapsed <=> spreaded', [
+                animate('0.5s')
+            ])
+        ]),
+        trigger('rightDown', [
+            state('collapsed', style({
+                transform: 'none'
+            })),
+            state('spreaded', style({
+                transform: 'rotate(90deg)'
+            })),
+            transition('collapsed <=> spreaded', [
+                animate('0.3s')
+            ])
+        ])
+    ]
 })
 export class ConfigurationPopupComponent implements OnInit {
     @HostBinding('class.font-opensans') fontFamily = true;
     @Output() outMenuClick = new EventEmitter<string>();
     @Output() outOpenLink = new EventEmitter<void>();
     @Input() canViewAdminMenu = false;
+    isCollapsed = false;
 
     constructor() {}
     ngOnInit() {}
@@ -20,5 +46,21 @@ export class ConfigurationPopupComponent implements OnInit {
 
     onOpenLink(): void {
         this.outOpenLink.emit();
+    }
+
+    toggleMenu(): void {
+        this.isCollapsed = !this.isCollapsed;
+    }
+
+    getCollapsedState(): string {
+        return this.isCollapsed ? 'collapsed' : 'spreaded';
+    }
+
+    isActive(linkElement: HTMLAnchorElement): boolean {
+        const listItem = linkElement.parentElement;
+
+        return Array.from(listItem.nextElementSibling.querySelectorAll('.l-link')).some((element: HTMLElement) => {
+            return element.classList.contains('active');
+        });
     }
 }
--- naver-web/src/main/webapp/v2/src/app/routes/config-page/config-page.component.ts
+++ naver-web/src/main/webapp/v2/src/app/routes/config-page/config-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { trigger, state, style, animate, transition } from '@angular/animations';
 
 import { UserPermissionCheckService, NewUrlStateNotificationService, UrlRouteManagerService } from 'app/shared/services';
 import { UrlPath } from 'app/shared/models';
@@ -6,19 +7,47 @@ import { UrlPath } from 'app/shared/models';
 @Component({
     selector: 'pp-config-page',
     templateUrl: './config-page.component.html',
-    styleUrls: ['./config-page.component.css']
+    styleUrls: ['./config-page.component.css'],
+    animations: [
+        trigger('collapseSpread', [
+            state('collapsed', style({
+                maxHeight: 0,
+                overflow: 'hidden'
+            })),
+            state('spreaded', style({
+                maxHeight: '300px'
+            })),
+            transition('collapsed <=> spreaded', [
+                animate('0.5s')
+            ])
+        ]),
+        trigger('rightDown', [
+            state('collapsed', style({
+                transform: 'none'
+            })),
+            state('spreaded', style({
+                transform: 'rotate(90deg)'
+            })),
+            transition('collapsed <=> spreaded', [
+                animate('0.3s')
+            ])
+        ])
+    ]
 })
 export class ConfigPageComponent implements OnInit {
     canViewAdminMenu: boolean;
+    isCollapsed = false;
+
     constructor(
         private urlRouteManagerService: UrlRouteManagerService,
         private newUrlStateNotificationService: NewUrlStateNotificationService,
-        private userPermissionCheckService: UserPermissionCheckService
+        private userPermissionCheckService: UserPermissionCheckService,
     ) {}
 
     ngOnInit() {
         this.canViewAdminMenu = this.userPermissionCheckService.canViewAdminMenu();
     }
+
     onClickExit(): void {
         const { startPath, pathParams, queryParams } = this.newUrlStateNotificationService.getPrevPageUrlInfo();
         const url = startPath === UrlPath.CONFIG ? [UrlPath.MAIN] : [startPath, ...[ ...pathParams.values() ]];
@@ -27,5 +56,21 @@ export class ConfigPageComponent implements OnInit {
         }, {});
 
         this.urlRouteManagerService.moveOnPage({ url, queryParam });
+    }
+
+    toggleMenu(): void {
+        this.isCollapsed = !this.isCollapsed;
+    }
+
+    getCollapsedState(): string {
+        return this.isCollapsed ? 'collapsed' : 'spreaded';
+    }
+
+    isActive(linkElement: HTMLAnchorElement): boolean {
+        const listItem = linkElement.parentElement;
+
+        return Array.from(listItem.nextElementSibling.querySelectorAll('.l-link')).some((element: HTMLElement) => {
+            return element.classList.contains('active');
+        });
     }
 }
