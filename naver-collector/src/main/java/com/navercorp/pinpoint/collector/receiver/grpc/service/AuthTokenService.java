9ebdd0fd8bdca23f8e4a3112fab67dbcde33a41d
koo-taejin <koo.taejin@navercorp.com>
2019-05-23 18:43:20 +0900

    [#oss2619] add token authentication function in grpc transport
    add token authentication function in grpc transport

---
+++ naver-collector/src/main/java/com/navercorp/pinpoint/collector/receiver/grpc/service/AuthTokenService.java
@@ -0,0 +1,151 @@
+/*
+ * Copyright 2019 NAVER Corp.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.navercorp.pinpoint.collector.receiver.grpc.service;
+
+import com.google.protobuf.ByteString;
+import com.google.protobuf.BytesValue;
+import com.google.protobuf.StringValue;
+import com.navercorp.pinpoint.collector.service.MetadataService;
+import com.navercorp.pinpoint.collector.service.TokenService;
+import com.navercorp.pinpoint.collector.vo.PaaSOrganizationInfo;
+import com.navercorp.pinpoint.collector.vo.PaaSOrganizationKey;
+import com.navercorp.pinpoint.collector.vo.Token;
+import com.navercorp.pinpoint.collector.vo.TokenCreateRequest;
+import com.navercorp.pinpoint.collector.vo.TokenType;
+import com.navercorp.pinpoint.common.util.StringUtils;
+import com.navercorp.pinpoint.grpc.auth.AuthGrpc;
+import com.navercorp.pinpoint.grpc.auth.PCmdGetTokenRequest;
+import com.navercorp.pinpoint.grpc.auth.PCmdGetTokenResponse;
+import com.navercorp.pinpoint.grpc.auth.PTokenResponseCode;
+import com.navercorp.pinpoint.grpc.auth.PTokenType;
+import com.navercorp.pinpoint.grpc.server.ServerContext;
+import io.grpc.stub.StreamObserver;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.net.InetSocketAddress;
+
+/**
+ * @author Taejin Koo
+ */
+@Service("authTokenService")
+@Profile("tokenAuthentication")
+public class AuthTokenService extends AuthGrpc.AuthImplBase {
+
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @Autowired
+    private TokenService tokenService;
+
+    @Autowired
+    private MetadataService metadataService;
+
+    @Override
+    public void getToken(PCmdGetTokenRequest request, StreamObserver<PCmdGetTokenResponse> responseObserver) {
+        // do not recrod request for security
+        logger.info("getToken() started");
+
+        try {
+            if (!verifyRequest(request)) {
+                doResponse(responseObserver, PTokenResponseCode.BAD_REQUEST);
+                return;
+            }
+
+            String licenseKey = request.getLicenseKey().getValue().toStringUtf8();
+            PaaSOrganizationKey paasKey = metadataService.selectPaaSOrganizationkey(licenseKey);
+            if (paasKey == null) {
+                doResponse(responseObserver, PTokenResponseCode.UNAUTHORIZED);
+                return;
+            }
+
+            String organization = paasKey.getOrganization();
+            PaaSOrganizationInfo organizationInfo = metadataService.selectPaaSOrganizationInfo(organization);
+            if (organizationInfo == null || !verifyPaaSOrganizationInfo(organizationInfo)) {
+                doResponse(responseObserver, PTokenResponseCode.INTERNAL_SERVER_ERROR);
+                return;
+            }
+
+            InetSocketAddress remoteAddress = ServerContext.getTransportMetadata().getRemoteAddress();
+
+            TokenCreateRequest tokenCreateRequest = new TokenCreateRequest(organizationInfo, TokenType.valueOf(request.getTokenType().name()), remoteAddress.getHostString());
+            Token token = tokenService.create(tokenCreateRequest);
+            doResponse(responseObserver, ByteString.copyFromUtf8(token.getKey()));
+        } catch (Exception e) {
+            logger.warn("Failed to handle createToken. message:{}", e.getMessage(), e);
+            doResponse(responseObserver, PTokenResponseCode.INTERNAL_SERVER_ERROR);
+        } finally {
+            responseObserver.onCompleted();
+        }
+    }
+
+    private void doResponse(StreamObserver<PCmdGetTokenResponse> responseObserver, PTokenResponseCode responseCode) {
+        doResponse(responseObserver, responseCode, responseCode.name(), null);
+    }
+
+    private void doResponse(StreamObserver<PCmdGetTokenResponse> responseObserver, ByteString tokenValue) {
+        doResponse(responseObserver, PTokenResponseCode.OK, PTokenResponseCode.OK.name(), tokenValue);
+    }
+
+    private void doResponse(StreamObserver<PCmdGetTokenResponse> responseObserver, PTokenResponseCode responseCode, String message, ByteString tokenValue) {
+        PCmdGetTokenResponse.Builder responseBuilder = PCmdGetTokenResponse.newBuilder();
+        responseBuilder.setCode(responseCode);
+        responseBuilder.setMessage(StringValue.of(message));
+        if (tokenValue != null) {
+            responseBuilder.setToken(BytesValue.of(tokenValue));
+        }
+        responseObserver.onNext(responseBuilder.build());
+    }
+
+
+    private boolean verifyRequest(PCmdGetTokenRequest request) {
+        String licenseKey = request.getLicenseKey().getValue().toStringUtf8();
+        if (StringUtils.isEmpty(licenseKey)) {
+            return false;
+        }
+
+        PTokenType tokenType = request.getTokenType();
+        TokenType type = TokenType.valueOf(tokenType.name());
+        if (type == TokenType.UNKNOWN) {
+            return false;
+        }
+
+        return true;
+    }
+
+    private boolean verifyPaaSOrganizationInfo(PaaSOrganizationInfo organizationInfo) {
+        String databaseName = organizationInfo.getDatabaseName();
+        if (databaseName == null) {
+            return false;
+        }
+
+        String hbaseNameSpace = organizationInfo.getHbaseNameSpace();
+        if (hbaseNameSpace == null) {
+            return false;
+        }
+
+        String organization = organizationInfo.getOrganization();
+        if (organization == null) {
+            return false;
+        }
+
+        return true;
+    }
+
+}
---
+++ naver-collector/src/test/java/com/navercorp/pinpoint/collector/handler/grpc/AuthTokenServiceTest.java
@@ -0,0 +1,196 @@
+/*
+ * Copyright 2019 NAVER Corp.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.navercorp.pinpoint.collector.handler.grpc;
+
+import com.google.protobuf.ByteString;
+import com.google.protobuf.BytesValue;
+import com.navercorp.pinpoint.collector.dao.MetadataDao;
+import com.navercorp.pinpoint.collector.dao.memory.MemoryMetadataDao;
+import com.navercorp.pinpoint.collector.receiver.grpc.service.AuthTokenService;
+import com.navercorp.pinpoint.collector.vo.PaaSOrganizationInfo;
+import com.navercorp.pinpoint.collector.vo.PaaSOrganizationKey;
+import com.navercorp.pinpoint.common.util.StringUtils;
+import com.navercorp.pinpoint.grpc.auth.PCmdGetTokenRequest;
+import com.navercorp.pinpoint.grpc.auth.PCmdGetTokenResponse;
+import com.navercorp.pinpoint.grpc.auth.PTokenResponseCode;
+import com.navercorp.pinpoint.grpc.auth.PTokenType;
+import com.navercorp.pinpoint.grpc.server.ServerContext;
+import com.navercorp.pinpoint.grpc.server.TransportMetadata;
+import io.grpc.Context;
+import io.grpc.stub.StreamObserver;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.net.InetSocketAddress;
+import java.util.UUID;
+
+/**
+ * @author Taejin Koo
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("classpath:applicationContext-tokenauth-test.xml")
+@ActiveProfiles("tokenAuthentication")
+public class AuthTokenServiceTest {
+
+    private static final String LICENSE_KEY = UUID.randomUUID().toString();
+    private static final String ORGANIZATION = "org";
+    private static final String NAMESPACE = "namespace";
+    private static final String REMOTE_ADDRESS = "127.0.0.1";
+
+    @Autowired
+    private AuthTokenService authTokenService;
+
+    @Autowired
+    private MetadataDao metadataDao;
+
+    @Before
+    public void setUp() throws Exception {
+        if (metadataDao instanceof MemoryMetadataDao) {
+            PaaSOrganizationKey paaSOrganizationKey = new PaaSOrganizationKey(LICENSE_KEY, ORGANIZATION);
+            ((MemoryMetadataDao) metadataDao).createPaaSOrganizationkey(LICENSE_KEY, paaSOrganizationKey);
+
+            PaaSOrganizationInfo paaSOrganizationInfo = new PaaSOrganizationInfo(ORGANIZATION, NAMESPACE, NAMESPACE);
+            ((MemoryMetadataDao) metadataDao).createPaaSOrganizationInfo(ORGANIZATION, paaSOrganizationInfo);
+        }
+    }
+
+    @Test
+    public void responseSuccessTest() {
+        TransportMetadata transportMetaData = createTransportMetaData(new InetSocketAddress(REMOTE_ADDRESS, 41413), 10);
+        attachContext(transportMetaData);
+
+        PCmdGetTokenRequest.Builder builder = PCmdGetTokenRequest.newBuilder();
+
+        builder.setLicenseKey(BytesValue.of(ByteString.copyFromUtf8(LICENSE_KEY)));
+        builder.setTokenType(PTokenType.SPAN);
+
+        RecordedStreamObserver<PCmdGetTokenResponse> recordedStreamObserver = new RecordedStreamObserver<>();
+        authTokenService.getToken(builder.build(), recordedStreamObserver);
+
+        PCmdGetTokenResponse latestResponse = recordedStreamObserver.getLatestResponse();
+        Assert.assertEquals(PTokenResponseCode.OK, latestResponse.getCode());
+        Assert.assertNotNull(latestResponse.getToken().getValue().toStringUtf8());
+    }
+
+
+    @Test
+    public void responseFailTest1() {
+        TransportMetadata transportMetaData = createTransportMetaData(new InetSocketAddress(REMOTE_ADDRESS, 41413), 10);
+        attachContext(transportMetaData);
+
+        PCmdGetTokenRequest.Builder builder = PCmdGetTokenRequest.newBuilder();
+
+        RecordedStreamObserver<PCmdGetTokenResponse> recordedStreamObserver = new RecordedStreamObserver<>();
+        authTokenService.getToken(builder.build(), recordedStreamObserver);
+
+        PCmdGetTokenResponse latestResponse = recordedStreamObserver.getLatestResponse();
+        Assert.assertEquals(PTokenResponseCode.BAD_REQUEST, latestResponse.getCode());
+        Assert.assertTrue(StringUtils.isEmpty(latestResponse.getToken().getValue().toStringUtf8()));
+    }
+
+    @Test
+    public void responseFailTest2() {
+        TransportMetadata transportMetaData = createTransportMetaData(new InetSocketAddress(REMOTE_ADDRESS, 41413), 10);
+        attachContext(transportMetaData);
+
+        PCmdGetTokenRequest.Builder builder = PCmdGetTokenRequest.newBuilder();
+        builder.setLicenseKey(BytesValue.of(ByteString.copyFromUtf8(LICENSE_KEY + "fail")));
+        builder.setTokenType(PTokenType.SPAN);
+
+        RecordedStreamObserver<PCmdGetTokenResponse> recordedStreamObserver = new RecordedStreamObserver<>();
+        authTokenService.getToken(builder.build(), recordedStreamObserver);
+
+        PCmdGetTokenResponse latestResponse = recordedStreamObserver.getLatestResponse();
+        Assert.assertEquals(PTokenResponseCode.UNAUTHORIZED, latestResponse.getCode());
+        Assert.assertTrue(StringUtils.isEmpty(latestResponse.getToken().getValue().toStringUtf8()));
+    }
+
+    private TransportMetadata createTransportMetaData(InetSocketAddress remoteAddress, long transportId) {
+        return new TransportMetadata() {
+            @Override
+            public InetSocketAddress getRemoteAddress() {
+                return remoteAddress;
+            }
+
+            @Override
+            public long getTransportId() {
+                return transportId;
+            }
+        };
+    }
+
+    private void attachContext(TransportMetadata transportMetadata) {
+        final Context currentContext = Context.current();
+        Context newContext = currentContext.withValue(ServerContext.getTransportMetadataKey(), transportMetadata);
+        newContext.attach();
+    }
+
+
+    private static class RecordedStreamObserver<T> implements StreamObserver<T> {
+
+        private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+        private int requestCount;
+        private T latestResponse;
+        private Throwable latestThrowable;
+        private boolean isCompleted = false;
+
+        @Override
+        public void onNext(T response) {
+            requestCount++;
+            logger.info("onNext:{}", response);
+            this.latestResponse = response;
+        }
+
+        @Override
+        public void onError(Throwable t) {
+            logger.info("onError. message:{}", t.getMessage(), t);
+            this.latestThrowable = t;
+        }
+
+        @Override
+        public void onCompleted() {
+            logger.info("onCompleted");
+            this.isCompleted = true;
+        }
+
+        public T getLatestResponse() {
+            return latestResponse;
+        }
+
+        public int getRequestCount() {
+            return requestCount;
+        }
+
+        public Throwable getLatestThrowable() {
+            return latestThrowable;
+        }
+
+        public boolean isCompleted() {
+            return isCompleted;
+        }
+    }
+
+}
