067e5b03f7dcf8ab5779d4591b7c9acb148f1169
minwoo.jung <minwoo.jung@navercorp.com>
2021-06-22 11:34:26 +0900

    [snapshot] add metric logic

--- web/src/main/java/com/navercorp/pinpoint/web/WebApp.java
+++ web/src/main/java/com/navercorp/pinpoint/web/WebApp.java
@@ -1,7 +1,6 @@
 package com.navercorp.pinpoint.web;
 
 import com.navercorp.pinpoint.common.server.util.ServerBootLogger;
-import com.navercorp.pinpoint.metric.web.MetricWebApp;
 import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.batch.BatchAutoConfiguration;
@@ -23,7 +22,7 @@ public class WebApp {
 
     public static void main(String[] args) {
         try {
-            WebStarter starter = new WebStarter(WebApp.class, WebMvcConfig.class, MetricWebApp.class);
+            WebStarter starter = new WebStarter(WebApp.class, WebMvcConfig.class);
             starter.start(args);
         } catch (Exception exception) {
             logger.error("[WebApp] could not launch app.", exception);
